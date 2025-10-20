ef2f6ce978ee8b95437760a86b7b059c39e65b91
Jerick-Service <jerickservice4@gmail.com>
2025-10-21 00:50:42 +0800

    Delete package_exercises directory

--- package_exercises/Main.py
+++
@@ -1,8 +0,0 @@
-from helpers.string import shout
-from helpers.math import area
-
-length = 5
-width = 8
-result = area(length, width)
-
-print(shout(f"The area of a {length}-by-{width} rectangle is {result}"))
