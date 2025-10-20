4325dfc29fb4f4813f5e62a9f331bade3d79220e
Jerick-Service <jerickservice4@gmail.com>
2025-10-21 00:46:32 +0800

    Update Main.py

--- package_exercises/Main.py
+++ package_exercises/Main.py
@@ -1,10 +1,8 @@
 from helpers.string import shout
 from helpers.math import area
 
-# Calculate the area
 length = 5
 width = 8
 result = area(length, width)
 
-# Print the result using shout
 print(shout(f"The area of a {length}-by-{width} rectangle is {result}"))
